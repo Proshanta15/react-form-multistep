903d34216e15979033fb09ed03e0f2643474e640
Proshanta Roy <71019355+Proshanta15@users.noreply.github.com>
2021-11-25 23:56:15 +0600

    deploy react multistep form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import UserForm from './components/UserForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
